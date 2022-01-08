a7c7fc7faf14609ebd6a6d8aa2a69b0b7f2b3c3b
magulloff <przemczar2001@gmail.com>
2022-01-08 19:35:43 +0100

    fix put request

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -22,7 +22,7 @@ impl Fairing for CORS {
 
     fn on_response(&self, request: &Request, response: &mut Response) {
         response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
-        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE"));
         response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
         response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
     }
