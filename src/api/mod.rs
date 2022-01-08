c86b5a9cee5a13aee7b4f1e0dffaa99401ebe0d5
magulloff <przemczar2001@gmail.com>
2022-01-08 19:24:31 +0100

    basic cors

--- src/api/mod.rs
+++ src/api/mod.rs
@@ -3,7 +3,30 @@ use dotenv::dotenv;
 use rocket_contrib::databases::diesel;
 use std::collections::HashMap;
 use rocket::config::{Config, Environment, Value};
+use rocket::http::Header;
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+
+
 pub mod entry;
+
+pub struct CORS;
+
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to responses",
+            kind: Kind::Response
+        }
+    }
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, PATCH, OPTIONS"));
+        response.set_header(Header::new("Access-Control-Allow-Headers", "*"));
+        response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+    }
+}
 
 #[database("alcalc-db")]
 pub struct DBPool(diesel::PgConnection);
@@ -30,6 +53,7 @@ pub fn init_routes() -> Rocket {
     ];
 
     rocket::custom(config)
+        .attach(CORS)
         .attach(DBPool::fairing())
         .mount("/entry", entry_routes)
 }
