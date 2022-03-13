0c80867e00d2dbc388d60dfbb39fcf0c2307d321
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-03-13 22:35:17 +0100

    get all users for admin only

--- src/sql_types.rs
+++ src/sql_types.rs
@@ -1,7 +1,7 @@
 use diesel_derive_enum::DbEnum;
 use serde::{Deserialize, Serialize};
 
-#[derive(Debug, Clone, Serialize, Deserialize, DbEnum)]
+#[derive(Debug, Clone, Serialize, Deserialize, DbEnum, PartialEq)]
 #[DieselType = "User_role"]
 #[PgType = "user_role"]
 pub enum UserRoles {
