b88fb95e307352987c01a13fb566cfc4a1f603cc
magulloff <przemczar2001@gmail.com>
2022-04-11 14:15:05 +0200

    a few more user tests

--- tests/db/entry.rs
+++ tests/db/entry.rs
@@ -27,4 +27,66 @@ fn insert_user_test(){
     })
 }
 
+#[test]
+fn get_by_id_test(){
+    let conn = establish_connection()
+        .unwrap_or_else(|e| panic!("Error establishing conncection: '{}'",e));
+    let test_new_user = NewUser {
+        name: "User1".to_string(),
+        email: "user1@mail.com".to_string()
+    };
+    let test_user = User::create_user(test_new_user);
+    let id = test_user.id;
+    let name = test_user.name.clone();
+
+    conn.test_transaction::<_,Error,_>(|| {
+        UserRepo::add_new(test_user, &conn).unwrap();
+        let user = UserRepo::get_by_id(id, &conn).unwrap();
+        assert_eq!(user.id,id);
+        assert_eq!(user.name.clone(),name);
+        Ok(())
+    })
+}
+
+#[test]
+fn delete_user_test() {
+    let conn = establish_connection()
+        .unwrap_or_else(|e| panic!("Error establishing conncection: '{}'",e));
+    let test_new_user = NewUser {
+        name: "User1".to_string(),
+        email: "user1@mail.com".to_string()
+    };
+    let test_user = User::create_user(test_new_user);
+    let id = test_user.id;
+    let name = test_user.name.clone();
+
+    conn.test_transaction::<_,Error,_>(|| {
+        UserRepo::add_new(test_user, &conn).unwrap();
+        let del_user = UserRepo::delete_user(id, &conn).unwrap();
+        assert_eq!(del_user.id,id);
+        assert_eq!(del_user.name.clone(),name);
+        Ok(())
+    })
+}
+
+#[test]
+fn get_by_email_test(){
+    let conn = establish_connection()
+        .unwrap_or_else(|e| panic!("Error establishing conncection: '{}'",e));
+    let test_new_user = NewUser {
+        name: "User1".to_string(),
+        email: "user1@mail.com".to_string()
+    };
+    let test_user = User::create_user(test_new_user);
+    let id = test_user.id;
+    let email = test_user.email.clone();
+
+    conn.test_transaction::<_,Error,_>(|| {
+        UserRepo::add_new(test_user, &conn).unwrap();
+        let user = UserRepo::get_by_email(email.clone(), &conn).unwrap();
+        assert_eq!(user.id,id);
+        assert_eq!(user.email.clone(),email);
+        Ok(())
+    })
+}
 
