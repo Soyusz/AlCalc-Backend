6ddf6096449740222e1df92bc829f5a62bad04ae
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-04-16 20:55:51 +0200

    remove warnings

--- src/services/jwt_token.rs
+++ src/services/jwt_token.rs
@@ -1,11 +1,8 @@
 use chrono::Utc;
-use jsonwebtoken::{
-    decode, encode, errors, Algorithm, DecodingKey, EncodingKey, Header, Validation,
-};
+use jsonwebtoken::{decode, encode, Algorithm, DecodingKey, EncodingKey, Header, Validation};
 use serde::de::DeserializeOwned;
 use serde::{Deserialize, Serialize};
 use std::env;
-use uuid::Uuid;
 
 #[derive(Deserialize, Serialize, Debug)]
 struct JwtToken<T> {
@@ -19,13 +16,13 @@ pub fn create<T: Serialize>(payload: T) -> Result<String, &'static str> {
         .checked_add_signed(chrono::Duration::weeks(10))
         .expect("valid timestamp")
         .timestamp();
-    let jwtToken = JwtToken {
+    let jwt_token = JwtToken {
         payload: payload,
         exp: expiration as usize,
     };
     let key = EncodingKey::from_secret(jwt_secret.as_bytes());
     let header = Header::new(Algorithm::HS512);
-    encode(&header, &jwtToken, &key).map_err(|_| "cannot create token")
+    encode(&header, &jwt_token, &key).map_err(|_| "cannot create token")
 }
 
 pub fn validate<T: DeserializeOwned>(token: String) -> Result<T, &'static str> {
