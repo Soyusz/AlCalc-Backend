4736464b8545234b7c86abda8677b5d8c729987d
Piotr Malatyński <piotrmalatynski1@gmail.com>
2022-05-20 23:42:06 +0200

    fix mutations

--- src/api/mutations/user.rs
+++ src/api/mutations/user.rs
@@ -1,16 +1,15 @@
-use crate::api::utils::structs::{AuthReturn, LoginCred, PhotoArg};
-use crate::api::utils::{auth_user::Auth, Response};
 use crate::api::utils::session::SessionTokenReturnType;
-use crate::model::session::Session;
-use crate::services::session as SessionService;
+use crate::api::utils::structs::{AuthReturn, LoginCred, PhotoArg};
+use crate::api::utils::Response;
 use crate::api::DBPool;
+use crate::model::session::Session;
 use crate::model::user::{NewUser, User as UserModel};
+use crate::services::session as SessionService;
 use crate::services::user as UserService;
+use crate::types::token::SessionToken;
 use rocket::response::status;
 use rocket::{get, post, put, routes, Route};
 use rocket_contrib::json::Json;
-
-
 
 #[post("/", format = "application/json", data = "<new_user>", rank = 1)]
 fn post_new(conn: DBPool, new_user: Json<NewUser>) -> Response<UserModel> {
@@ -30,7 +29,7 @@ fn login(conn: DBPool, login_cred: Json<LoginCred>) -> Response<SessionTokenRetu
     Ok(())
         .map(|_| login_cred.into_inner())
         .and_then(|login_cred| SessionService::login(login_cred, &conn))
-        .map(|token| Json(SessionTokenReturnType{ token }))
+        .map(|token| Json(SessionTokenReturnType { token }))
         .map_err(|e| status::BadRequest(Some(e)))
 }
 
@@ -42,11 +41,13 @@ fn login_invalid() -> status::BadRequest<&'static str> {
 #[put("/photo", format = "application/json", data = "<arg>")]
 fn put_photo(
     arg: Json<PhotoArg>,
-    auth: Auth,
+    session_token: SessionToken,
     conn: DBPool,
 ) -> Result<Option<String>, status::BadRequest<&'static str>> {
-    Ok(arg.into_inner())
-        .and_then(|b| UserService::update_photo(b.photo, auth.user_id, &conn))
+    SessionService::is_authorized(session_token.session_id, &conn)
+        .and_then(|session| {
+            UserService::update_photo(arg.into_inner().photo, session.user_id, &conn)
+        })
         .map(|r| r.photo)
         .map_err(|e| status::BadRequest(Some(e)))
 }
