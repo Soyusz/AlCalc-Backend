8237f84a72ba590b687e8b26cef151756521882e
magulloff <przemczar2001@gmail.com>
2022-05-30 03:10:55 +0200

    code cleaning

--- src/services/image.rs
+++ src/services/image.rs
@@ -17,11 +17,8 @@ pub fn create_from_blob(value: Vec<u8>, conn: &DBPool) -> Result<Image, &'static
 pub fn create_from_base(base: String, conn: &DBPool) -> Result<Image, &'static str> {
     DataUrl::process(base.as_str())
         .map_err(|_| "Error processing data-url")
-        .and_then(|u| u.decode_to_vec().map_err(|_| "Error decoding to vector"))
-        .and_then(|x| {
-            let body = x.0;
-            create_from_blob(body, conn)
-        })
+        .and_then(|url| url.decode_to_vec().map_err(|_| "Error decoding to vector"))
+        .and_then(|body| create_from_blob(body.0, conn))
 }
 
 pub fn gen_link(image: Image) -> String {
